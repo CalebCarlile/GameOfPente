1ac196ef1718eab122634920b470eeaacd7230c1
Evan Lamb <evanl1719@gmail.com>
2018-05-15 11:46:37 -0600

    Fixed an error in testing with Wes

--- GameOfPente/Assets/Tests/NewTestScript.cs
+++ GameOfPente/Assets/Tests/NewTestScript.cs
@@ -3,7 +3,8 @@ using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
 
-public class NewTestScript {
+public class NewTestScript 
+{
 
     [Test]
     public void NewTestScriptSimplePasses() 
