90a409491a01ce8109fb300095c0a77550464809
Caleb Carlile <calebcarlile@SpartanBook.hsd1.ut.comcast.net>
2018-05-16 10:18:57 -0600

    Node class stubs

---
+++ GameOfPente/Assets/Scripts/Node.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+	public enum eColor
+	{
+		WHITE,
+		BLACK,
+		EMPTY
+	}
+
+	public enum Direction
+	{
+		NORTH,
+		EAST,
+		SOUTH,
+		WEST
+	}
+
+	public eColor Color {get; set;}
+
+	public Node NorthNode;
+	public Node EastNode;
+	public Node SouthNode;
+	public Node WestNode;
+
+	public Node GetNode(Direction dir)
+	{
+		switch (dir)
+		{
+			case Direction.NORTH:
+
+			break;
+			case Direction.EAST:
+
+			break;
+			case Direction.SOUTH:
+
+			break;
+			case Direction.WEST:
+
+			break;
+		}
+	}
+
+	public void SetNode(Direction dir, Node node)
+	{
+
+	}
+
+}
