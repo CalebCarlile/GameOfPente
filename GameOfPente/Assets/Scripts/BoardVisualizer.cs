05e94107a7ce7534c8be1fff04574ba8713ff58c
Evan Lamb <evanl1719@gmail.com>
2018-05-16 10:47:18 -0600

    Started Board Visualizer

---
+++ GameOfPente/Assets/Scripts/BoardVisualizer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardVisualizer : MonoBehaviour 
+{
+	[SerializeField] int xDimension;
+	[SerializeField] int yDimension;
+	[SerializeField] float distanceBetween;
+	[SerializeField] Color lineColor;
+	
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
