16b2867f7ddd8a67c94234fc7c7295a6ff00e937
Wesley Barlow <sirwesleybarlow@gmail.com>
2018-05-17 11:52:31 -0600

    Work on capture, tria, tessera, and win condition functions

--- GameOfPente/Assets/Scripts/BoardManager.cs
+++ GameOfPente/Assets/Scripts/BoardManager.cs
@@ -44,15 +44,19 @@ public class BoardManager : Singleton<BoardManager>
 
     public void CheckBoard(Node last)
     {
+        if (WonGame(last))
+        {
+            print(last.Color + " WON!");
+        }
         foreach (Node capture in FindCaptures(last))
         {
             capture.Color = eColor.EMPTY;
         }
-        if (CallTessera(last))
+        if (TesseraCreated(last))
         {
             print("TESSERA!");
         }
-        else if (CallTria(last))
+        else if (TriaCreated(last))
         {
             print("TRIA!");
         }
@@ -97,15 +101,63 @@ public class BoardManager : Singleton<BoardManager>
         return lines;
     }
 
-    public bool CallTria(Node last)
+    private bool IsUnblocked(Line line)
+    {
+        Node previous = line.nodes[0].GetNode(line.opposite);
+        if (previous == null || previous.Color != eColor.EMPTY)
+        {
+            return false;
+        }
+        Node next = line.nodes[line.nodes.Count - 1].GetNode(line.direction);
+        if (next == null || next.Color != eColor.EMPTY)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public bool TriaCreated(Node last)
     {
         List<Line> triaLines = GetLines(last, 3);
+        List<List<eColor>> trias = new List<List<eColor>>() {
+            new List<eColor>() { last.Color, last.Color, last.Color },
+            new List<eColor>() { last.Color, last.Color, eColor.EMPTY, last.Color },
+            new List<eColor>() { last.Color, eColor.EMPTY, last.Color, last.Color }
+        };
+        foreach (Line line in triaLines)
+        {
+            for (int i = 0; i < line.nodes.Count - 4; i++)
+            {
+                List<Node> nodeLine = line.nodes.GetRange(0, 3);
+                List<eColor> colors = new List<eColor>();
+                foreach (Node node in nodeLine)
+                {
+                    colors.Add(node.Color);
+                }
+                if (trias.Contains(colors))
+                {
+                    if (IsUnblocked(new Line() { nodes = nodeLine, direction = line.direction, opposite = line.opposite }))
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
         return false;
     }
 
-    public bool CallTessera(Node last)
+    public bool TesseraCreated(Node last)
     {
         List<Line> tesseraLines = GetLines(last, 4);
+        List<eColor> tessera = new List<eColor>() { last.Color, last.Color, last.Color, last.Color };
+
+        return false;
+    }
+
+    public bool WonGame(Node last)
+    {
+        List<Line> winLines = GetLines(last, 5);
+        List<eColor> win = new List<eColor>() { last.Color, last.Color, last.Color, last.Color, last.Color };
         return false;
     }
 
