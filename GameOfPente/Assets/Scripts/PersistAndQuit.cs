0e945ed05f169990cd2cd8cec0b0af508a4b66ec
Evan Lamb <evanl1719@gmail.com>
2018-05-18 10:24:52 -0600

    Added quit option that persists throughout the game

---
+++ GameOfPente/Assets/Scripts/PersistAndQuit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistAndQuit : MonoBehaviour 
+{
+	void Awake()
+	{
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	void Update()
+	{
+		if(Input.GetKeyUp(KeyCode.Space))
+		{
+			Application.Quit();
+		}
+	}
+
+}
